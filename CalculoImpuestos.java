1adbef76e2e0fca8b15b56ace42b4e458cd5f1d9
Daniel Ortiz Joachin <31485342+DanJoachinn@users.noreply.github.com>
2018-04-06 14:48:22 -0500

    Add files via upload

--- CalculoImpuestos.java
+++ CalculoImpuestos.java
@@ -24,9 +24,9 @@ public class CalculoImpuestos {
 	            aguinaldoExento,
 	            aguinaldoGravado,
 	            primaVacacional,
-	            primaVacionalExenta,
+	            primaVacacionalExenta,
 	            primaVacacionalGravada,
-	            totalIngresisGravados,
+	            totalIngresosGravados,
 	            medicosHospitales,
 	            funerarios,
 	            SGMM,
@@ -38,25 +38,32 @@ public class CalculoImpuestos {
 	            maximoDeducibleColegiatura,
 	            totalDeducciones,
 	            deduccionPermitida,
-	            TotalPagar;
-	        
-	            
-	            
-	            
+	            totalPagar,
+	            sueldoConDeduccion,
+	            cuotaFija,
+	            excedente,
+	            porcentajeExcendete;
 
 	public CalculoImpuestos(File datos) {
 		try {
 			this.br=new BufferedReader(new FileReader(datos));
 			this.datosStr=this.br.readLine().split(",");
 			Calcular(this.datosStr);
+			total();
 			crearArchivo();
 			this.br.close();
 		} catch (FileNotFoundException e1) {
-			JOptionPane.showMessageDialog(null, this, "No se pudo encontar el archivo", 0);
+			JOptionPane.showMessageDialog(null,"No se pudo encontar el archivo");
 		} catch (IOException e1) {
-			JOptionPane.showMessageDialog(null, this, "Hubo un error con el archivo", 0);
+			JOptionPane.showMessageDialog(null, "Hubo un error con el archivo");
 			
 		}
+	}
+	
+	public CalculoImpuestos(String [] datos) {
+		Calcular(datos);
+		total();
+		crearArchivo();
 	}
 	
 	public void Calcular(String[] datos) {
@@ -65,12 +72,26 @@ public class CalculoImpuestos {
 		this.sueldoMensual=Integer.parseInt(datos[2]);
 		this.sueldoAnual=this.sueldoMensual*12;
 		this.aguinaldo=Integer.parseInt(datos[3]);
-		this.aguinaldoExento=this.sueldoMensual/2;
-		this.aguinaldoGravado=this.aguinaldo-this.aguinaldoExento;
+		if(this.aguinaldo>(this.sueldoMensual/2)) {
+			this.aguinaldoExento=this.sueldoMensual/2f;
+			this.aguinaldoGravado=this.aguinaldo-this.aguinaldoExento;
+		}
+		else {
+			this.aguinaldoExento=this.aguinaldo;
+			this.aguinaldoGravado=0;
+		}
+		
 		this.primaVacacional=Integer.parseInt(datos[4]);
-		this.primaVacionalExenta=1209f;
-		this.primaVacacionalGravada=this.primaVacacional-this.primaVacionalExenta;
-		this.totalIngresisGravados=this.sueldoAnual+this.aguinaldoGravado+this.primaVacacionalGravada;
+		if(this.primaVacacional>1209f) {
+			this.primaVacacionalExenta=1209f;
+			this.primaVacacionalGravada=this.primaVacacional-this.primaVacacionalExenta;
+		}
+		else {
+			this.primaVacacionalExenta=this.primaVacacional;
+			this.primaVacacionalGravada=0;
+		}
+		
+		this.totalIngresosGravados=this.sueldoAnual+this.aguinaldoGravado+this.primaVacacionalGravada;
 		this.medicosHospitales=Integer.parseInt(datos[5]);
 		this.funerarios=Integer.parseInt(datos[6]);
 		this.SGMM=Integer.parseInt(datos[7]);
@@ -78,49 +99,122 @@ public class CalculoImpuestos {
 		this.donativos=Integer.parseInt(datos[9]);
 		this.retiro=Integer.parseInt(datos[10]);
 		this.transporteEscolar=Integer.parseInt(datos[11]);
-		this.nivelEscolar=datos[12];
+		this.nivelEscolar=datos[12].toLowerCase();
 		this.colegiatura=Integer.parseInt(datos[13]);
 		
-		if(this.nivelEscolar.equals("Preescolar")) {
+		float colegiaturadeducida;
+		if(this.nivelEscolar.equals("preescolar")) {
 			this.maximoDeducibleColegiatura=14200f;
+			colegiaturadeducida=14200f;
 		}
-		else if(this.nivelEscolar.equals("Primaria")) {
+		else if(this.nivelEscolar.equals("primaria")) {
 			this.maximoDeducibleColegiatura=12900f;
+			colegiaturadeducida=12900f;
 		}
-		else if(this.nivelEscolar.equals("Secundaria")) {
+		else if(this.nivelEscolar.equals("secundaria")) {
 			this.maximoDeducibleColegiatura=19900f;
+			colegiaturadeducida=19900f;
 		}
-		else if(this.nivelEscolar.equals("Profesional técnico")) {
+		else if(this.nivelEscolar.equals("profesional técnico")) {
 			this.maximoDeducibleColegiatura=17100f;
+			colegiaturadeducida=17100f;
 		}
-		else if(this.nivelEscolar.equals("Bachillerato")||this.nivelEscolar.equals("Preparatoria")) {
+		else if(this.nivelEscolar.equals("bachillerato")||this.nivelEscolar.equals("preparatoria")) {
 			this.maximoDeducibleColegiatura=24500f;
+			colegiaturadeducida=24500f;
+		}
+		else {
+			this.maximoDeducibleColegiatura=0f;
+			colegiaturadeducida=0f;
 		}
 		
-		if(this.maximoDeducibleColegiatura<this.colegiatura) {
-			this.colegiatura=this.maximoDeducibleColegiatura;
+		if(this.colegiatura<this.maximoDeducibleColegiatura) {
+			colegiaturadeducida=this.colegiatura;
 		}
 		
-		this.totalDeducciones=this.medicosHospitales+this.funerarios+this.SGMM+this.hipoteca+this.donativos+this.transporteEscolar+this.colegiatura;
-		this.deduccionPermitida=(this.sueldoAnual+this.aguinaldo+this.primaVacacional)/10;
+		this.totalDeducciones=this.medicosHospitales+this.funerarios+this.SGMM+this.hipoteca+this.donativos+this.transporteEscolar+colegiaturadeducida;
 		
-		if(this.totalDeducciones>this.deduccionPermitida) {
-			this.totalDeducciones=this.deduccionPermitida;
+		float retiroDeducible;
+		if(this.retiro<(this.aguinaldo+this.sueldoAnual+this.primaVacacional)/10f) {
+			retiroDeducible=this.retiro;
 		}
-		if(this.retiro>this.deduccionPermitida) {
-			this.retiro=this.deduccionPermitida;
+		else {
+			retiroDeducible=(this.aguinaldo+this.sueldoAnual+this.primaVacacional)/10f;
 		}
+		
+		this.deduccionPermitida=this.totalDeducciones+retiroDeducible;
+		this.sueldoConDeduccion=this.totalIngresosGravados-this.deduccionPermitida;
 	}
 	
 	public void crearArchivo() {
 		try {
-			PrintWriter pw=new PrintWriter(new FileWriter("resultados.csv"));
-			pw.println("Nombre,RFC,Sueldo mensual,Ingreso anual,Aguinaldo,Aguinaldo exento,Aguinaldo gravado,Prima vacacional,Prima vacacional excenta,Prima vacacional gravada,Total ingresos gravan,Medicos y hospitales,Gastos funerarios,SGMM,Hipotecarios,Donativos,Subcuenta retiro,Transporte escolar,Nivel educativo,Maximo a deducir colegiatura,Colegiatura pagada,Total deducciones (sin retiro),Deduccion permitida 10%,Motal a pagar");
-			pw.println(this.nombre+","+this.RFC+","+this.sueldoMensual+","+this.sueldoAnual+","+this.aguinaldo+","+this.aguinaldoExento+","+this.aguinaldoGravado+","+this.primaVacacional+","+this.primaVacionalExenta+","+this.primaVacacionalGravada+","+this.totalIngresisGravados+","+this.medicosHospitales+","+this.funerarios+","+this.SGMM+","+this.hipoteca+","+this.donativos+","+this.retiro+","+this.transporteEscolar+","+this.nivelEscolar+","+this.maximoDeducibleColegiatura+","+this.colegiatura+","+this.totalDeducciones+","+this.deduccionPermitida+","+this.TotalPagar);
+			PrintWriter pw=new PrintWriter(new FileWriter("resultadosISR.csv"));
+			pw.println("Nombre,RFC,Sueldo mensual,Ingreso anual,Aguinaldo,Aguinaldo exento,Aguinaldo gravado,Prima vacacional,Prima vacacional excenta,Prima vacacional gravada,Total ingresos gravan,Medicos y hospitales,Gastos funerarios,SGMM,Hipotecarios,Donativos,Subcuenta retiro,Transporte escolar,Nivel educativo,Maximo a deducir colegiatura,Colegiatura pagada,Total deducciones (sin retiro),Deduccion permitida (10% + 10% retiro),Monto calculo de ISR,Cuota fija,Porcentaje excedente,Pago excedente,Total a pagar");
+			pw.println(this.nombre+","+this.RFC+","+this.sueldoMensual+","+this.sueldoAnual+","+this.aguinaldo+","+this.aguinaldoExento+","+this.aguinaldoGravado+","+this.primaVacacional+","+this.primaVacacionalExenta+","+this.primaVacacionalGravada+","+this.totalIngresosGravados+","+this.medicosHospitales+","+this.funerarios+","+this.SGMM+","+this.hipoteca+","+this.donativos+","+this.retiro+","+this.transporteEscolar+","+this.nivelEscolar+","+this.maximoDeducibleColegiatura+","+this.colegiatura+","+this.totalDeducciones+","+this.deduccionPermitida+","+this.sueldoConDeduccion+","+this.cuotaFija+","+this.porcentajeExcendete+","+this.excedente+","+this.totalPagar);
 			pw.close();
+			JOptionPane.showMessageDialog(null, "Se ha creado el archivo con el resumen y resultados");
 		}
 		catch(IOException e) {
-			System.out.println("No se pudo escribir en el archivo");
+			JOptionPane.showMessageDialog(null, "No se pudo escribir el archivo");
 		}	}
 	
+	public void total() {
+		if(this.sueldoConDeduccion>=.01 && this.sueldoConDeduccion<=5952.84) {
+			this.cuotaFija=0;
+			this.excedente=(this.sueldoConDeduccion-.01f)*.0192f;
+			this.porcentajeExcendete=1.92f;
+		}
+		else if(this.sueldoConDeduccion>=5952.85 && this.sueldoConDeduccion<=50524.92) {
+			this.cuotaFija=114.29f;
+			excedente=(this.sueldoConDeduccion-5952.85f)*.064f;
+			this.porcentajeExcendete=6.4f;
+		}
+		else if(this.sueldoConDeduccion>=50524.93 && this.sueldoConDeduccion<=88793.04) {
+			this.cuotaFija=2966.91f;
+			this.excedente=(this.sueldoConDeduccion-50524.93f)*.1088f;
+			this.porcentajeExcendete=10.88f;
+		}
+		else if(this.sueldoConDeduccion>=88793.05 && this.sueldoConDeduccion<=103218) {
+			this.cuotaFija=7130.48f;
+			this.excedente=(this.sueldoConDeduccion-88793.05f)*.16f;
+			this.porcentajeExcendete=16f;
+		}
+		else if(this.sueldoConDeduccion>=103218.01 && this.sueldoConDeduccion<=123580.20) {
+			this.cuotaFija=9438.47f;
+			this.excedente=(this.sueldoConDeduccion-103218f)*.1792f;
+			this.porcentajeExcendete=17.92f;
+		}
+		else if(this.sueldoConDeduccion>=123580.21 && this.sueldoConDeduccion<=249243.48) {
+			this.cuotaFija=13087.37f;
+			this.excedente=(this.sueldoConDeduccion-123580.21f)*.2136f;
+			this.porcentajeExcendete=21.36f;
+		}
+		else if(this.sueldoConDeduccion>=249243.49 && this.sueldoConDeduccion<=392841.96) {
+			this.cuotaFija=39929.05f;
+			this.excedente=(this.sueldoConDeduccion-249243.49f)*.2352f;
+			this.porcentajeExcendete=23.52f;
+		}
+		else if(this.sueldoConDeduccion>=392841.97 && this.sueldoConDeduccion<=750000) {
+			this.cuotaFija=73703.41f;
+			this.excedente=(this.sueldoConDeduccion-392841.97f)*.30f;
+			this.porcentajeExcendete=30f;
+		}
+		else if(this.sueldoConDeduccion>=750000.01 && this.sueldoConDeduccion<=1000000) {
+			this.cuotaFija=180850.82f;
+			this.excedente=(this.sueldoConDeduccion-750000.01f)*.32f;
+			this.porcentajeExcendete=32f;
+		}
+		else if(this.sueldoConDeduccion>=1000000.01 && this.sueldoConDeduccion<=3000000) {
+			this.cuotaFija=260850.81f;
+			this.excedente=(this.sueldoConDeduccion-1000000.01f)*.34f;
+			this.porcentajeExcendete=34f;
+		}
+		else{
+			this.cuotaFija=940850.81f;
+			this.excedente=(this.sueldoConDeduccion-3000000.01f)*.35f;
+			this.porcentajeExcendete=35f;
+		}
+		
+		this.totalPagar=this.cuotaFija+this.excedente;
+	}
 }
--- ImpuestosDatos.java
+++ ImpuestosDatos.java
@@ -1,7 +1,12 @@
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -19,14 +24,20 @@ public class ImpuestosDatos extends JPanel {
 						tfHipoteca,
 						tfRetiro,
 						tfTransporteEscolar,
-						tfNivelEducativo,
 						tfColegiaturaTotal;
+	
+	private JButton btCalcular;
+	
+	private JComboBox cbNivelEscolar;
+	
+	private String[] datos =new String[14];
 	
 	
 
 	public ImpuestosDatos() {
 		super();
-		this.setLayout(new GridLayout(14,2,10,10));
+		String[] educacion= {"Ninguno","Preescolar","Primaria","Secundaria","Profesional técnico","Preparatoria"};
+		this.setLayout(new GridLayout(15,2,10,10));
 		this.setPreferredSize(new Dimension(400,600));
 		this.tfNombre=new JTextField(10);
 		this.tfRFC=new JTextField(10);
@@ -40,14 +51,32 @@ public class ImpuestosDatos extends JPanel {
 		this.tfDonativos=new JTextField(10);
 		this.tfRetiro=new JTextField(10);
 		this.tfTransporteEscolar=new JTextField(10);
-		this.tfNivelEducativo=new JTextField(10);
 		this.tfColegiaturaTotal=new JTextField(10);
+		this.btCalcular=new JButton("Calcular");
+		
+		this.cbNivelEscolar=new JComboBox(educacion);
+		
+		this.btCalcular.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					validacion();
+					hacerArray();
+					CalculoImpuestos ci=new CalculoImpuestos(datos);
+				}
+				catch(Exception arg0) {
+					JOptionPane.showMessageDialog(null, "Los valores estan mal ingresados. Ingreselos de nuevo");
+				}
+			}
+			
+		});
 		
 		this.add(new JLabel("Nombre: "));
 		this.add(this.tfNombre);
-		this.add(new JLabel("RFC"));
+		this.add(new JLabel("RFC: "));
 		this.add(this.tfRFC);
-		this.add(new JLabel("Sueldo Mensual"));
+		this.add(new JLabel("Sueldo Mensual: "));
 		this.add(this.tfSueldoMensual);
 		this.add(new JLabel("Aguinal Recibido: "));
 		this.add(this.tfAguinaldo);
@@ -68,9 +97,102 @@ public class ImpuestosDatos extends JPanel {
 		this.add(new JLabel("Transporte Escolar: "));
 		this.add(this.tfTransporteEscolar);
 		this.add(new JLabel("Nivel Educativo: "));
-		this.add(this.tfNivelEducativo);
+		this.add(this.cbNivelEscolar);
 		this.add(new JLabel("Colegiatura Total: "));
 		this.add(this.tfColegiaturaTotal);
+		this.add(this.btCalcular);
+		
+	}
+	
+	public void validacion() throws Exception {
+		int validacion;
+		if(!this.tfSueldoMensual.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfSueldoMensual.getText());
+		}
+			
+		if(!this.tfAguinaldo.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfAguinaldo.getText());
+		}
+		if(!this.tfPrimaVacacional.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfPrimaVacacional.getText());
+		}
+		
+		if(!this.tfMedicosHospitales.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfMedicosHospitales.getText());
+		}
+		if(!this.tfFunerarios.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfFunerarios.getText());
+		}
+		if(!this.tfSGMM.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfSGMM.getText());
+		}
+		
+		if(!this.tfHipoteca.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfHipoteca.getText());
+		}
+		if(!this.tfDonativos.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfDonativos.getText());
+		}
+		if(!this.tfRetiro.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfRetiro.getText());
+		}
+		if(!this.tfTransporteEscolar.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfTransporteEscolar.getText());
+		}
+		if(!this.tfColegiaturaTotal.getText().equals("")) {
+			validacion=Integer.parseInt(this.tfColegiaturaTotal.getText());
+		}
+	}
+	
+	public void hacerArray() {
+		this.datos[0]=this.tfNombre.getText();
+		this.datos[1]=this.tfRFC.getText();
+		this.datos[2]=this.tfSueldoMensual.getText();
+		if(this.datos[2].equals("")||this.datos[2]==null) {
+			this.datos[2]="0";
+		}
+		this.datos[3]=this.tfAguinaldo.getText();
+		if(this.datos[3].equals("")||this.datos[3]==null) {
+			this.datos[3]="0";
+		}
+		this.datos[4]=this.tfPrimaVacacional.getText();
+		if(this.datos[4].equals("")||this.datos[4]==null) {
+			this.datos[4]="0";
+		}
+		this.datos[5]=this.tfMedicosHospitales.getText();
+		if(this.datos[5].equals("")||this.datos[5]==null) {
+			this.datos[5]="0";
+		}
+		this.datos[6]=this.tfFunerarios.getText();
+		if(this.datos[6].equals("")||this.datos[6]==null) {
+			this.datos[6]="0";
+		}
+		this.datos[7]=this.tfSGMM.getText();
+		if(this.datos[7].equals("")||this.datos[7]==null) {
+			this.datos[7]="0";
+		}
+		this.datos[8]=this.tfHipoteca.getText();
+		if(this.datos[8].equals("")||this.datos[8]==null) {
+			this.datos[8]="0";
+		}
+		this.datos[9]=this.tfDonativos.getText();
+		if(this.datos[9].equals("")||this.datos[9]==null) {
+			this.datos[9]="0";
+		}
+		this.datos[10]=this.tfRetiro.getText();
+		if(this.datos[10].equals("")||this.datos[10]==null) {
+			this.datos[10]="0";
+		}
+		this.datos[11]=this.tfTransporteEscolar.getText();
+		if(this.datos[11].equals("")||this.datos[11]==null) {
+			this.datos[11]="0";
+		}
+		this.datos[12]=(String) this.cbNivelEscolar.getSelectedItem();
+		
+		this.datos[13]=this.tfColegiaturaTotal.getText();
+		if(this.datos[13].equals("")||this.datos[13]==null) {
+			this.datos[13]="0";
+		}
 		
 	}
 	
